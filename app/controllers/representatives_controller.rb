898ab814e6b1a96b711ac0649d3a6a954c25086f
Diraj Thajali <diraj.thajali@berkeley.edu>
2022-11-18 23:34:44 +0000

    [feat] added show.html for representatives

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class RepresentativesController < ApplicationController
+  before_action :set_representative, only: %i[show]
+
   def index
     @representatives = Representative.all
   end
+
+  def set_representative
+    @representative = Representative.find(
+      params[:id]
+    )
+  end
+
 end
