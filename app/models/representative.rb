506cbf9c63270fe11f6e4866ed8876910b573064
Aidan Garde <aidan.garde@berkeley.edu>
2022-11-22 03:45:10 +0000

    fixedbug

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -16,7 +16,15 @@ class Representative < ApplicationRecord
           ocdid_temp = office.division_id
         end
       end
+      #ADDED IN for Part 1
+      if Representative.where(name: official.name).exists?
+        rep = Representative.where(name: official.name)[0]
+        reps.push(rep)
+        next
+      end
 
+
+      #END
       rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
           title: title_temp })
       reps.push(rep)
